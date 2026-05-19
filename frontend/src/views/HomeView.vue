5a72e628ff306e240d949cf9a84eb09b7abcffe4
Premdeep Maiti <23f1001231@ds.study.iitm.ac.in>
2026-05-19 10:42:09 +0530

    Added HomeView and Setup Router

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+    <h1 class="text-4xl font-bold text-blue-600 mb-4">This is home view.</h1>
+    <p class="text-gray-700">This is home view.</p>
+  </div>
+</template>
+
+<style scoped></style>
