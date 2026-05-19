d5554f9433ec921b66f87946b64ae7978338e063
Premdeep Maiti <23f1001231@ds.study.iitm.ac.in>
2026-05-19 10:15:07 +0530

    added vue, tailwind

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+    <h1 class="text-4xl font-bold text-blue-600 mb-4">You did it!</h1>
+    <p class="text-gray-700">
+      Visit
+      <a
+        href="https://vuejs.org/"
+        target="_blank"
+        rel="noopener"
+        class="text-blue-500 hover:underline font-medium"
+      >
+        vuejs.org
+      </a>
+      to read the documentation
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
